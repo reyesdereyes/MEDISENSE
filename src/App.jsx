80b661c997086c4b37b6df8e142499a048ece3c7
reyesdereyes <jesus.enrique0z05@gmail.com>
2025-11-26 22:20:40 -0400

    mejora en css

--- src/App.jsx
+++ src/App.jsx
@@ -3,23 +3,20 @@ import Header from './components/Header.jsx'; // Importación corregida en App.j
 import './App.css';
 import Inicio from './pages/Inicio.jsx';
 import Login from './pages/Login.jsx'; 
-import Registro from './pages/registro.jsx';
-
+import Registro from './pages/Registro.jsx'; // Mayúscula R para coincidir con archivo
 
 function App() {
   return (
-    // BrowserRouter debe envolver toda la aplicación para habilitar las rutas
     <BrowserRouter>
-        {/* Header se queda fuera de <Routes> para ser visible en todas las páginas */}
-        <Routes>
-            {/* Ruta de inicio */}
-            <Route path="/" element={<Inicio/>} /> 
-            <Route path="/login" element={<Login/>} />
-            <Route path="/registro" element={<Registro/>} />
-            {/* Aquí agregarías más rutas: /medicos, /consulta-ia, etc. */}
-        </Routes>
+      <Header /> {/* Header fuera de Routes para estar visible siempre */}
+      <Routes>
+        <Route path="/" element={<Inicio />} /> 
+        <Route path="/login" element={<Login />} />
+        <Route path="/registro" element={<Registro />} />
+        {/* Otras rutas aquí */}
+      </Routes>
     </BrowserRouter>
   )
 }
 
-export default App
+export default App;
