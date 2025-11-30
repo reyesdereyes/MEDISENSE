151463ab16a1f83ea3ffcac04aa9ccc79329d9e9
reyesdereyes <jesus.enrique0z05@gmail.com>
2025-11-30 18:34:56 -0400

    mejora en css

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,21 @@
-import { BrowserRouter, Routes, Route, Router } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import './App.css';
 import Login from './pages/Login.jsx'; 
 import Principal from './pages/Principal.jsx';
 import Register from './pages/Register.jsx';
-import Conte from './pages/conte.jsx';
 
 
 function App() {
   return (
-    // BrowserRouter debe envolver toda la aplicación para habilitar las rutas
-    <BrowserRouter>
-        {/* Header se queda fuera de <Routes> para ser visible en todas las páginas */}
-        <Routes>
-            {/* Ruta de inicio */}
-            <Route path="/" element={<Principal/>} /> 
-            <Route path="/login" element={<Login/>} />
-            <Route path="/registro" element={<Register/>} />
-            <Route path="/MEDISENSE" element={<Conte/>} />
-            {/* Aquí agregarías más rutas: /medicos, /consulta-ia, etc. */}
-        </Routes>
-    </BrowserRouter>
-  )
+    <>
+      <Routes>
+        <Route path="/" element={<Principal />} /> 
+        <Route path="/login" element={<Login />} />
+        <Route path="/registro" element={<Register />} />
+      
+      </Routes>
+    </>
+  );
 }
 
-export default App
+export default App;
